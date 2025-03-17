52828ffc73ffa30c199e598e6cc56eeaa52de852
fernandezsteven <fernandstevengod@gmail.com>
2025-03-17 19:54:34 +0800

    updating and adding some functions of scripts and html

--- resources/css-js/script.js
+++ resources/css-js/script.js
@@ -1,1 +1,9 @@
-const name = "hello"
+const list = document.getElementById('list');
+const menu = document.getElementById('menu');
+const bars = document.querySelector('.fa-bars');
+
+menu.addEventListener('click', () => {
+    list.classList.toggle('translate-x-0');
+    bars.classList.toggle('fa-xmark');
+});
+
