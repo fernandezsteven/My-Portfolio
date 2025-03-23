5877ca8e7e5fecb117d1784d88c8b723d64488a8
fernandezsteven <fernandstevengod@gmail.com>
2025-03-23 17:20:45 +0800

    updating the functions to script.js

--- resources/css-js/script.js
+++ resources/css-js/script.js
@@ -1,11 +1,12 @@
 const list = document.getElementById('list');
 const menu = document.getElementById('menu');
 const bars = document.querySelector('.fa-bars');
-
+// toggle the list when the menu is clicked
 menu.addEventListener('click', () => {
     list.classList.toggle('translate-x-0');
     bars.classList.toggle('fa-xmark');
-});
+}); 
+
 
 // create a function to download the resume via window open
 document.getElementById("download").onclick = function() {
