aea472102c99d86f748f5b92b3e084f29a243728
Steven Godfrey Fernandez <fernandstevengod@gmail.com>
2025-04-06 17:51:49 +0800

    updating some files in my portfolios

--- resources/css-js/script.js
+++ resources/css-js/script.js
@@ -1,14 +1,1 @@
-const list = document.getElementById('list');
-const menu = document.getElementById('menu');
-const bars = document.querySelector('.fa-bars');
-// toggle the list when the menu is clicked
-menu.addEventListener('click', () => {
-    list.classList.toggle('translate-x-0');
-    bars.classList.toggle('fa-xmark');
-}); 
-
-
-// create a function to download the resume via window open
-document.getElementById("download").onclick = function() {
-    window.open("resources/files/resume.pdf");
-}
+console.log("Steven Godfrey H. Fernandez");
