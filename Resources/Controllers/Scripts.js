b06f8fb433740b9fd955ea04679e0bbd9f70b535
Steven Godfrey Fernandez <fernandstevengod@gmail.com>
2025-06-13 16:20:30 +0800

    fix the issue the script file under resources file

--- Resources/Controllers/Scripts.js
+++ Resources/Controllers/Scripts.js
@@ -114,7 +114,7 @@ const form = document.forms['submit-to-google-sheet']
   form.addEventListener('submit', e => {
     e.preventDefault()
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => console.log('Success!', response) form.reset())
+      .then(response => console.log('Success!', response))
         // .then(response => {
         //     msg.innerHTML = "Message sent successfully"
         //     setTimeout(function(){
